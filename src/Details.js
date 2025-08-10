a22c87c108686e39f1bce512acf69a890591a203
Niraj1996abcd <sanjaynks1996@gmail.com>
2025-08-10 21:38:07 +0530

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,127 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/nirajImage.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Niraj Kumar",
+  tagline: "I am a React Js Developer",
+  img: profile,
+  about: `React.js Developer with 2+ years building scalable web applications. Expert in React.js, Redux Toolkit, JavaScript ES6+,
+  and RESTful API integration.
+  `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/niraj-kumar-91b511265/",
+  github: "https://github.com/Niraj1996abcd",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Sineor React Js Developer",
+    Company: `Dealers Auto Center (USA)`,
+    Location: "Working in Noida",
+    Type: "Full Time",
+    Duration: "Feb 2024 - Present",
+  },
+  {
+    Position: "React Js Developer",
+    Company: `Valentine Saga`,
+    Location: "Dwarka Delhi",
+    Type: "Full Time",
+    Duration: "February 2023 - December 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Technology, Electrical Engineering",
+    Company: `Giani Zail Singh Campus College of Engineering & Technology Bathinda(Punjab)`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2016 - JUly 2020",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  // sass: sass,
+  // tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  // figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Dealers auto center",
+    image: projectImage1,
+    //     description: `This is sample project description random things are here in description This is sample
+    // project lorem ipsum generator for dummy content`,
+    //     techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://dealersautocenter.com/",
+    // githubLink: "https://github.com",
+  },
+  {
+    title: "Dealers Themes",
+    image: projectImage2,
+    //     description: `This is sample project description random things are here in description This is sample
+    // project lorem ipsum generator for dummy content`,
+    //     techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://unitedmotors.us/",
+    // githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "nirajstu163@gmail.com",
+  phone: "+91-8969668638",
+};
